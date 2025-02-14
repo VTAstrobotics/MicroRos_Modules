7b6f6e22995c414cff7c0ba15470c5d745fd39e0
cameron <ckiw23@gmail.com>
2025-02-14 23:48:14 +0000

    maybe someone can make this work, I can't figure out why it deosn't

--- force_example1.c
+++ force_example1.c
@@ -49,71 +49,17 @@ void fx29_init() {
     gpio_pull_up(SCL_PIN);
 }
 
-int reg_write(i2c_inst_t *i2c, 
-                const uint addr, 
-                const uint8_t reg, 
-                uint8_t *buf,
-                const uint8_t nbytes);
-
-int reg_read(   i2c_inst_t *i2c,
-                const uint addr,
-                const uint8_t reg,
-                uint8_t *buf,
-                const uint8_t nbytes);
-
-int reg_write(  i2c_inst_t *i2c, 
-                const uint addr, 
-                const uint8_t reg, 
-                uint8_t *buf,
-                const uint8_t nbytes) {
-
-    int num_bytes_read = 0;
-    uint8_t msg[nbytes + 1];
-
-    // Check to make sure caller is sending 1 or more bytes
-    if (nbytes < 1) {
-        return 0;
-    }
-
-    // Append register address to front of data packet
-    msg[0] = reg;
-    for (int i = 0; i < nbytes; i++) {
-        msg[i + 1] = buf[i];
-    }
-
-    // Write data to register(s) over I2C
-    i2c_write_blocking(i2c, addr, msg, (nbytes + 1), false);
-
-    return num_bytes_read;
-}
-
-// Read byte(s) from specified register. If nbytes > 1, read from consecutive
-// registers.
-int reg_read(  i2c_inst_t *i2c,
-                const uint addr,
-                const uint8_t reg,
-                uint8_t *buf,
-                const uint8_t nbytes) {
-
-    int num_bytes_read = 0;
-
-    // Check to make sure caller is asking for 1 or more bytes
-    if (nbytes < 1) {
-        return 0;
-    }
-
-    // Read data from register(s) over I2C
-    i2c_write_blocking(i2c, addr, &reg, 1, true);
-    num_bytes_read = i2c_read_blocking(i2c, addr, buf, nbytes, false);
-
-    return num_bytes_read;
-}
 
 int fx29_read_force_raw() {
 
     // Address reading
-    uint8_t data[6];
-    return reg_read(I2C_PORT, FX29_I2C_ADDR, REG_DEVID, data, 1);
+    uint8_t buf[2];
+    i2c_write_blocking(i2c0, FX29_I2C_ADDR, 0x00, 2, true );
+    buf[0] = 0;
+    buf[1] = 0;
+    i2c_read_blocking(i2c0, FX29_I2C_ADDR, buf, 2, false);
+    int force = (buf[0] << 8) | buf[1];
+    return force;
 }
 
 float fx29_convert_to_lbf(int raw_force) {
