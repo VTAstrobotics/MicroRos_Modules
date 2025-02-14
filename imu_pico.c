bdab4027eae49f5a2b98b0e622bd3ac54a9129ee
cameron <ckiw23@gmail.com>
2025-02-14 22:48:51 +0000

    fixed sensor code

--- imu_pico.c
+++ imu_pico.c
@@ -27,6 +27,7 @@
 #define MPU6050_REG_POWER_MGMT_1  0x6B
 #define MPU6050_REG_ACCEL_XOUT_H  0x3B
 #define MPU6050_REG_GYRO_XOUT_H   0x43
+static int addr = 0x68;
 
 
 static rcl_publisher_t imu_publisher;
@@ -37,18 +38,18 @@ static rcl_allocator_t allocator;
 static rclc_support_t support;
 static rclc_executor_t executor;
 
-static void i2c_init_imu(void)
-{
-    i2c_init(I2C_PORT, 400 * 1000);
+static void mpu6050_reset() {
+    // Two byte reset. First byte register, second byte data
+    // There are a load more options to set up the device in different ways that could be added here
+    uint8_t buf[] = {0x6B, 0x80};
+    i2c_write_blocking(i2c_default, addr, buf, 2, false);
+    sleep_ms(100); // Allow device to reset and stabilize
 
-    gpio_set_function(I2C_SDA_PIN, GPIO_FUNC_I2C); //using i2c
-    gpio_set_function(I2C_SCL_PIN, GPIO_FUNC_I2C);
-    gpio_pull_up(I2C_SDA_PIN);
-    gpio_pull_up(I2C_SCL_PIN);
-
-    sleep_ms(100);
+    // Clear sleep mode (0x6B register, 0x00 value)
+    buf[1] = 0x00;  // Clear sleep mode by writing 0x00 to the 0x6B register
+    i2c_write_blocking(i2c_default, addr, buf, 2, false); 
+    sleep_ms(10); // Allow stabilization after waking up
 }
-
 
 static void i2c_write_byte(uint8_t dev_addr, uint8_t reg_addr, uint8_t data)
 {
@@ -70,44 +71,72 @@ static void i2c_read_bytes(uint8_t dev_addr, uint8_t reg_addr, uint8_t *data, si
 
 static void imu_setup(void)
 {
-    // Wake up the MPU6050 from sleep
-    i2c_write_byte(MPU6050_ADDRESS, MPU6050_REG_POWER_MGMT_1, 0x00);
-    sleep_ms(100);
+    // Two byte reset. First byte register, second byte data
+    // There are a load more options to set up the device in different ways that could be added here
+    uint8_t buf[] = {0x6B, 0x80};
+    i2c_write_blocking(i2c_default, addr, buf, 2, false);
+    sleep_ms(100); // Allow device to reset and stabilize
+
+    // Clear sleep mode (0x6B register, 0x00 value)
+    buf[1] = 0x00;  // Clear sleep mode by writing 0x00 to the 0x6B register
+    i2c_write_blocking(i2c_default, addr, buf, 2, false); 
+    sleep_ms(10); // Allow stabilization after waking up
+
 }
 
-static void imu_read_raw(int16_t* accel_x, int16_t* accel_y, int16_t* accel_z,
-                         int16_t* gyro_x,  int16_t* gyro_y,  int16_t* gyro_z)
+static void imu_read_raw(int16_t accel[3], int16_t gyro[3], int16_t *temp)
 {
    
-    uint8_t accel_buf[6];
-    i2c_read_bytes(MPU6050_ADDRESS, MPU6050_REG_ACCEL_XOUT_H, accel_buf, 6);
+    // For this particular device, we send the device the register we want to read
+    // first, then subsequently read from the device. The register is auto incrementing
+    // so we don't need to keep sending the register we want, just the first.
 
-    *accel_x = (accel_buf[0] << 8) | accel_buf[1];
-    *accel_y = (accel_buf[2] << 8) | accel_buf[3];
-    *accel_z = (accel_buf[4] << 8) | accel_buf[5];
+    uint8_t buffer[6];
 
-    uint8_t gyro_buf[6];
-    i2c_read_bytes(MPU6050_ADDRESS, MPU6050_REG_GYRO_XOUT_H, gyro_buf, 6);
+    // Start reading acceleration registers from register 0x3B for 6 bytes
+    uint8_t val = 0x3B;
+    i2c_write_blocking(i2c_default, addr, &val, 1, true); // true to keep master control of bus
+    i2c_read_blocking(i2c_default, addr, buffer, 6, false);
 
-    *gyro_x = (gyro_buf[0] << 8) | gyro_buf[1];
-    *gyro_y = (gyro_buf[2] << 8) | gyro_buf[3];
-    *gyro_z = (gyro_buf[4] << 8) | gyro_buf[5];
+    for (int i = 0; i < 3; i++) {
+        accel[i] = (buffer[i * 2] << 8 | buffer[(i * 2) + 1]);
+    }
+
+    // Now gyro data from reg 0x43 for 6 bytes
+    // The register is auto incrementing on each read
+    val = 0x43;
+    i2c_write_blocking(i2c_default, addr, &val, 1, true);
+    i2c_read_blocking(i2c_default, addr, buffer, 6, false);  // False - finished with bus
+
+    for (int i = 0; i < 3; i++) {
+        gyro[i] = (buffer[i * 2] << 8 | buffer[(i * 2) + 1]);;
+    }
+
+    // Now temperature from reg 0x41 for 2 bytes
+    // The register is auto incrementing on each read
+    val = 0x41;
+    i2c_write_blocking(i2c_default, addr, &val, 1, true);
+    i2c_read_blocking(i2c_default, addr, buffer, 2, false);  // False - finished with bus
+
+    *temp = buffer[0] << 8 | buffer[1];
 }
 
 static void imu_fill_message(sensor_msgs__msg__Imu *msg)
 {
+    int16_t acceleration[3], gyro[3], temp;
+
     int16_t ax_raw, ay_raw, az_raw;
     int16_t gx_raw, gy_raw, gz_raw;
 
-    imu_read_raw(&ax_raw, &ay_raw, &az_raw, &gx_raw, &gy_raw, &gz_raw);
+    imu_read_raw(acceleration, gyro, &temp);
 
-    float ax = ax_raw / 16384.0f * 9.80665f; // from g to m/s^2
-    float ay = ay_raw / 16384.0f * 9.80665f;
-    float az = az_raw / 16384.0f * 9.80665f;
+    float ax = acceleration[0] / 16384.0f * 9.80665f; // from g to m/s^2
+    float ay = acceleration[1] / 16384.0f * 9.80665f;
+    float az = acceleration[2] / 16384.0f * 9.80665f;
 
-    float gx = (gx_raw / 131.0f) * (3.14159265359f / 180.0f);
-    float gy = (gy_raw / 131.0f) * (3.14159265359f / 180.0f);
-    float gz = (gz_raw / 131.0f) * (3.14159265359f / 180.0f);
+    float gx = (gyro[0] / 131.0f) * (3.14159265359f / 180.0f);
+    float gy = (gyro[1] / 131.0f) * (3.14159265359f / 180.0f);
+    float gz = (gyro[2] / 131.0f) * (3.14159265359f / 180.0f);
 
     msg->orientation.x = 0.0;
     msg->orientation.y = 0.0;
@@ -155,11 +184,15 @@ int main()
 
 
     stdio_init_all();
+    i2c_init(i2c_default, 400 * 1000);
+    gpio_set_function(PICO_DEFAULT_I2C_SDA_PIN, GPIO_FUNC_I2C);
+    gpio_set_function(PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C);
+    gpio_pull_up(PICO_DEFAULT_I2C_SDA_PIN);
+    gpio_pull_up(PICO_DEFAULT_I2C_SCL_PIN);
+    // Make the I2C pins available to picotool
 
 
-    i2c_init_imu();
-    imu_setup();
-
+    mpu6050_reset(); 
 
     const int timeout_ms = 1000;  // 1 second per attempt
     const uint8_t attempts = 120; // up to 120 seconds
